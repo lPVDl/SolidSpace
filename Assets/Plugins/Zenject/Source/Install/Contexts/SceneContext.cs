00de4f0a49835063bef5a2e37baa9cceedd9acca
Victor Fuchadzhy <pvd1313@gmail.com>
2021-04-17 12:26:53 +0300

    Simplify scene context

--- Assets/Plugins/Zenject/Source/Install/Contexts/SceneContext.cs
+++ Assets/Plugins/Zenject/Source/Install/Contexts/SceneContext.cs
@@ -8,22 +8,11 @@ using ModestTree.Util;
 using UnityEngine;
 using UnityEngine.Serialization;
 using Zenject.Internal;
-using UnityEngine.Events;
 
 namespace Zenject
 {
     public class SceneContext : RunnableContext
     {
-        public event Action PreInstall;
-        public event Action PostInstall;
-        public event Action PreResolve;
-        public event Action PostResolve;
-
-        public UnityEvent OnPreInstall;
-        public UnityEvent OnPostInstall;
-        public UnityEvent OnPreResolve;
-        public UnityEvent OnPostResolve;
-
         public static Action<DiContainer> ExtraBindingsInstallMethod;
         public static Action<DiContainer> ExtraBindingsLateInstallMethod;
 
@@ -45,24 +34,12 @@ namespace Zenject
 
         DiContainer _container;
 
-        readonly List<SceneDecoratorContext> _decoratorContexts = new List<SceneDecoratorContext>();
-
         bool _hasInstalled;
         bool _hasResolved;
 
         public override DiContainer Container
         {
             get { return _container; }
-        }
-
-        public bool HasResolved
-        {
-            get { return _hasResolved; }
-        }
-
-        public bool HasInstalled
-        {
-            get { return _hasInstalled; }
         }
 
         public bool IsValidating
@@ -190,21 +167,6 @@ namespace Zenject
             return parentContainers;
         }
 
-        List<SceneDecoratorContext> LookupDecoratorContexts()
-        {
-            if (_contractNames.IsEmpty())
-            {
-                return new List<SceneDecoratorContext>();
-            }
-
-            return UnityUtil.AllLoadedScenes
-                .Except(gameObject.scene)
-                .SelectMany(scene => scene.GetRootGameObjects())
-                .SelectMany(root => root.GetComponentsInChildren<SceneDecoratorContext>())
-                .Where(decoratorContext => _contractNames.Contains(decoratorContext.DecoratedContractName))
-                .ToList();
-        }
-
         public void Install()
         {
             Assert.That(!_hasInstalled);
@@ -213,167 +175,43 @@ namespace Zenject
             Assert.IsNull(_container);
 
             var parents = GetParentContainers();
-            Assert.That(!parents.IsEmpty());
-            Assert.That(parents.All(x => x.IsValidating == parents.First().IsValidating));
 
             _container = new DiContainer(parents, parents.First().IsValidating);
 
-            // Do this after creating DiContainer in case it's needed by the pre install logic
-            if (PreInstall != null)
-            {
-                PreInstall();
-            }
-
-            if (OnPreInstall != null)
-            {
-                OnPreInstall.Invoke();
-            }
-
-            Assert.That(_decoratorContexts.IsEmpty());
-            _decoratorContexts.AddRange(LookupDecoratorContexts());
-
-            if (_parentNewObjectsUnderSceneContext)
-            {
-                _container.DefaultParent = transform;
-            }
-            else
-            {
-                _container.DefaultParent = null;
-            }
-
-            // Record all the injectable components in the scene BEFORE installing the installers
-            // This is nice for cases where the user calls InstantiatePrefab<>, etc. in their installer
-            // so that it doesn't inject on the game object twice
-            // InitialComponentsInjecter will also guarantee that any component that is injected into
-            // another component has itself been injected
-            var injectableMonoBehaviours = new List<MonoBehaviour>();
-            GetInjectableMonoBehaviours(injectableMonoBehaviours);
-            foreach (var instance in injectableMonoBehaviours)
-            {
-                _container.QueueForInject(instance);
-            }
-
-            foreach (var decoratorContext in _decoratorContexts)
-            {
-                decoratorContext.Initialize(_container);
-            }
+            _container.DefaultParent = transform;
 
             _container.IsInstalling = true;
 
             try
             {
-                InstallBindings(injectableMonoBehaviours);
+                InstallBindings();
             }
             finally
             {
                 _container.IsInstalling = false;
             }
-
-            if (PostInstall != null)
-            {
-                PostInstall();
-            }
-
-            if (OnPostInstall != null)
-            {
-                OnPostInstall.Invoke();
-            }
         }
 
         public void Resolve()
         {
-            if (PreResolve != null)
-            {
-                PreResolve();
-            }
-
-            if (OnPreResolve != null)
-            {
-                OnPreResolve.Invoke();
-            }
-
-            Assert.That(_hasInstalled);
-            Assert.That(!_hasResolved);
             _hasResolved = true;
 
             _container.ResolveRoots();
-
-            if (PostResolve != null)
-            {
-                PostResolve();
-            }
-
-            if (OnPostResolve != null)
-            {
-                OnPostResolve.Invoke();
-            }
         }
 
-        void InstallBindings(List<MonoBehaviour> injectableMonoBehaviours)
+        void InstallBindings()
         {
             _container.Bind(typeof(Context), typeof(SceneContext)).To<SceneContext>().FromInstance(this);
-            _container.BindInterfacesTo<SceneContextRegistryAdderAndRemover>().AsSingle();
-
-            // Add to registry first and remove from registry last
-            _container.BindExecutionOrder<SceneContextRegistryAdderAndRemover>(-1);
-
-            foreach (var decoratorContext in _decoratorContexts)
-            {
-                decoratorContext.InstallDecoratorSceneBindings();
-            }
-
-            InstallSceneBindings(injectableMonoBehaviours);
 
             _container.Bind(typeof(SceneKernel), typeof(MonoKernel))
                 .To<SceneKernel>().FromNewComponentOn(gameObject).AsSingle().NonLazy();
 
-            _container.Bind<ZenjectSceneLoader>().AsSingle();
-
-            if (ExtraBindingsInstallMethod != null)
-            {
-                ExtraBindingsInstallMethod(_container);
-                // Reset extra bindings for next time we change scenes
-                ExtraBindingsInstallMethod = null;
-            }
-
-            // Always install the installers last so they can be injected with
-            // everything above
-            foreach (var decoratorContext in _decoratorContexts)
-            {
-                decoratorContext.InstallDecoratorInstallers();
-            }
-
             InstallInstallers();
-
-            foreach (var decoratorContext in _decoratorContexts)
-            {
-                decoratorContext.InstallLateDecoratorInstallers();
-            }
-
-            if (ExtraBindingsLateInstallMethod != null)
-            {
-                ExtraBindingsLateInstallMethod(_container);
-                // Reset extra bindings for next time we change scenes
-                ExtraBindingsLateInstallMethod = null;
-            }
         }
 
         protected override void GetInjectableMonoBehaviours(List<MonoBehaviour> monoBehaviours)
         {
-            var scene = gameObject.scene;
-
-            ZenUtilInternal.AddStateMachineBehaviourAutoInjectersInScene(scene);
-            ZenUtilInternal.GetInjectableMonoBehavioursInScene(scene, monoBehaviours);
-        }
-
-        // These methods can be used for cases where you need to create the SceneContext entirely in code
-        // Note that if you use these methods that you have to call Run() yourself
-        // This is useful because it allows you to create a SceneContext and configure it how you want
-        // and add what installers you want before kicking off the Install/Resolve
-        public static SceneContext Create()
-        {
-            return CreateComponent<SceneContext>(
-                new GameObject("SceneContext"));
+            monoBehaviours.Clear();
         }
     }
 }
