c543bd4ffb6c505bca910786fa9536f79a0fc199
Victor Fuchadzhy <pvd1313@gmail.com>
2021-05-12 14:34:33 +0300

    Fix profiling is broken after moving to assembly

--- Assets/SolidSpace/Scripts/Profiling/Runtime/Installers/ProfilingInstaller.cs
+++ Assets/SolidSpace/Scripts/Profiling/Runtime/Installers/ProfilingInstaller.cs
@@ -1,14 +1,15 @@
+using SolidSpace.DependencyInjection;
 using UnityEngine;
 
 namespace SolidSpace.Profiling
 {
-    public class ProfilingInstaller : ScriptableObject
+    public class ProfilingInstaller : ScriptableObjectInstaller
     {
         [SerializeField] private ProfilingConfig _config;
         
-        // public override void InstallBindings(IContainer container)
-        // {
-        //     container.Bind<ProfilingManager>(_config);
-        // }
+        public override void InstallBindings(IDependencyContainer container)
+        {
+            container.Bind<ProfilingManager>(_config);
+        }
     }
 }
