8c8475a55f6bc793d2a58c49692a5f812b5ea003
Victor Fuchadzhy <pvd1313@gmail.com>
2021-05-15 17:45:52 +0300

    Move resume to root. Add build.

---
+++ .resume/Scripts/Examples/AtlasMath.cs
@@ -0,0 +1,20 @@
+using System.Runtime.CompilerServices;
+using SolidSpace.Entities.World;
+using SolidSpace.Mathematics;
+using Unity.Mathematics;
+
+namespace SolidSpace.Entities.Rendering.Atlases
+{
+    public struct AtlasMath
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public int2 ComputeOffset(AtlasChunk chunk, AtlasIndex index)
+        {
+            chunk.GetPower(out var indexPower, out var itemPower);
+            var x = chunk.offsetX << 2 + (index.itemId & ((1 << indexPower) - 1)) * (1 << itemPower);
+            var y = chunk.offsetY << 2 + (index.itemId >> indexPower) * (1 << itemPower);
+
+            return new int2(x, y);
+        }
+    }
+}
