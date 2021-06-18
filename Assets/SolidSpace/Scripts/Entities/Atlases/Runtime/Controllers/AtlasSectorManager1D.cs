be3710ec3aa288aa25af0f30ace1a1f8e5e16763
Victor Fuchadzhy <pvd1313@gmail.com>
2021-06-18 17:25:54 +0300

    [SOL-34] Make health atlas sectors join when released

--- Assets/SolidSpace/Scripts/Entities/Atlases/Runtime/Controllers/AtlasSectorManager1D.cs
+++ Assets/SolidSpace/Scripts/Entities/Atlases/Runtime/Controllers/AtlasSectorManager1D.cs
@@ -5,20 +5,20 @@ namespace SolidSpace.Entities.Atlases
 {
     public class AtlasSectorManager1D
     {
-        private readonly Stack<ushort>[] _emptySectors;
+        private readonly List<ushort>[] _emptySectors;
         private readonly byte _atlasPower;
         
         public AtlasSectorManager1D(int atlasSize)
         {
             _atlasPower = (byte) Math.Ceiling(Math.Log(atlasSize, 2));
-            _emptySectors = new Stack<ushort>[_atlasPower + 1];
+            _emptySectors = new List<ushort>[_atlasPower + 1];
             
             for (var i = 0; i <= _atlasPower; i++)
             {
-                _emptySectors[i] = new Stack<ushort>();
+                _emptySectors[i] = new List<ushort>();
             }
             
-            _emptySectors[_atlasPower].Push(0);
+            _emptySectors[_atlasPower].Add(0);
         }
 
         public ushort Allocate(int power)
@@ -26,7 +26,11 @@ namespace SolidSpace.Entities.Atlases
             var sectorStack = _emptySectors[power];
             if (sectorStack.Count > 0)
             {
-                return sectorStack.Pop();
+                var lastIndex = sectorStack.Count - 1;
+                var sector = sectorStack[lastIndex];
+                sectorStack.RemoveAt(lastIndex);
+
+                return sector;
             }
 
             for (var i = power + 1; i <= _atlasPower; i++)
@@ -37,12 +41,15 @@ namespace SolidSpace.Entities.Atlases
                     continue;
                 }
 
-                var sector = sectorStack.Pop();
+                var lastIndex = sectorStack.Count - 1;
+                var sector = sectorStack[lastIndex];
+                sectorStack.RemoveAt(lastIndex);
+                
                 for (var j = i - 1; j >= power; j--)
                 {
-                    var size = 1 << (j - 1);
+                    var size = 1 << (j - 2);
                     sectorStack = _emptySectors[j];
-                    sectorStack.Push((ushort) (sector + size));
+                    sectorStack.Add((ushort) (sector + size));
                 }
 
                 return sector;
@@ -53,8 +60,33 @@ namespace SolidSpace.Entities.Atlases
 
         public void Release(ushort offset, int power)
         {
-            // TODO: [SOL-34]: Make atlas restore & join sectors  when chunks are released.
-            _emptySectors[power].Push(offset);
+            var stack = _emptySectors[power];
+            var stackCount = stack.Count;
+            if (stackCount == 0)
+            {
+                stack.Add(offset);
+                return;
+            }
+
+            var size = 1 << (power - 2);
+            var parentSize = size << 1;
+            var atlasSize = 1 << (_atlasPower - 2);
+            var leftIsOdd = (atlasSize - offset) % parentSize == 0;
+            var rightIsOdd = (atlasSize - offset + parentSize) % parentSize == 0;
+            var requiredDelta = (leftIsOdd & rightIsOdd) ? size : -size;
+            for (var i = 0; i < stackCount; i++)
+            {
+                var siblingOffset = stack[i];
+                var siblingDelta = siblingOffset - offset;
+                if (siblingDelta == requiredDelta)
+                {
+                    stack.RemoveAt(i);
+                    Release(Math.Min(offset, siblingOffset), power + 1);
+                    return;
+                }
+            }
+            
+            stack.Add(offset);
         }
     }
 }
