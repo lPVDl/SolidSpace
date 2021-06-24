0e178a512378558738f2d5e7ec98304367b8af8e
Victor Fuchadzhy <pvd1313@gmail.com>
2021-06-24 20:09:54 +0300

    [SOL-73] Make AI ship use backward thrusters. Make AI keep distance offset.

--- Assets/SolidSpace/Scripts/Entities/Actors/Jobs/ActorControlJob.cs
+++ Assets/SolidSpace/Scripts/Entities/Actors/Jobs/ActorControlJob.cs
@@ -1,5 +1,4 @@
 using System;
-using SolidSpace.Debugging;
 using SolidSpace.Entities.Components;
 using SolidSpace.Mathematics;
 using Unity.Burst;
@@ -7,7 +6,6 @@ using Unity.Collections;
 using Unity.Entities;
 using Unity.Jobs;
 using Unity.Mathematics;
-using UnityEngine;
 
 namespace SolidSpace.Entities.Actors
 {
@@ -16,6 +14,7 @@ namespace SolidSpace.Entities.Actors
     {
         private const float Acceleration = 30f;
         private const float RotationSpeed = FloatMath.TwoPI * 0.2f;
+        private const float ApproachDistanceOffset = 100f;
 
         [ReadOnly] public NativeArray<ArchetypeChunk> inArchetypeChunks;
         [ReadOnly] public float2 inSeekPosition;
@@ -48,7 +47,7 @@ namespace SolidSpace.Entities.Actors
                 var currentVelocity = velocities[i].value;
                 var targetDirection = inSeekPosition - currentPosition;
                 var targetDirectionNormalized = FloatMath.Normalize(targetDirection);
-                var targetDistance = FloatMath.Magnitude(targetDirection);
+                var targetDistance = FloatMath.Magnitude(targetDirection) - ApproachDistanceOffset;
 
                 var currentRotation = rotations[i].value;
                 FloatMath.SinCos(currentRotation, out var dirSin, out var dirCos);
@@ -57,17 +56,23 @@ namespace SolidSpace.Entities.Actors
                 var currentAngle = rotations[i].value;
                 var distanceOverVelocity = targetDistance / Math.Max(1f, FloatMath.Magnitude(currentVelocity));
                 var targetImpulse = targetDirectionNormalized * Acceleration * distanceOverVelocity - currentVelocity;
-                var impulseAngle = FloatMath.Atan2(targetImpulse);
 
+                var impulseDot = FloatMath.Dot(targetImpulse, targetDirection);
+                var impulseAngle = FloatMath.Atan2(targetImpulse);
+                if (impulseDot < 0)
+                {
+                    impulseAngle += FloatMath.PI;
+                }
+                
                 var deltaAngle = FloatMath.DeltaAngle(currentAngle, impulseAngle);
-                var thrusterPower = 1 - Math.Min(1, Math.Abs(deltaAngle) / (FloatMath.PI * 0.5f));
-                var impulse = currentDirection * (inDeltaTime * thrusterPower * Acceleration);
+                var thrusterPower = 1f - 2f * Math.Abs(deltaAngle) / FloatMath.PI;
+                var impulse = currentDirection * (inDeltaTime * thrusterPower * Acceleration * Math.Sign(impulseDot));
 
                 velocities[i] = new VelocityComponent
                 {
                     value = currentVelocity + impulse
                 };
-
+                
                 rotations[i] = new RotationComponent
                 {
                     value = FloatMath.MoveAngleTowards(currentAngle, impulseAngle, inDeltaTime * RotationSpeed)
