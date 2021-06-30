a12ef0e35c04c1547004c3f0c45993978fb83277
Victor Fuchadzhy <pvd1313@gmail.com>
2021-06-30 16:53:09 +0300

    [SOL-128] Move collision resolving to separate class.

--- Assets/SolidSpace/Scripts/Entities/Physics/Rigidbody/Jobs/RigidbodyCollisionJob.cs
+++ Assets/SolidSpace/Scripts/Entities/Physics/Rigidbody/Jobs/RigidbodyCollisionJob.cs
@@ -13,20 +13,6 @@ namespace SolidSpace.Entities.Physics.Rigidbody
     [BurstCompile]
     public struct RigidbodyCollisionJob : IJobParallelFor
     {
-        private struct ColliderShape
-        {
-            public float2 center;
-            public float rotation;
-            public float2 size;
-
-            public ColliderShape(float2 center, float rotation, float2 size)
-            {
-                this.center = center;
-                this.rotation = rotation;
-                this.size = size;
-            }
-        }
-        
         [ReadOnly] public BakedColliders inColliders;
         [ReadOnly] public NativeArray<ArchetypeChunk> inArchetypeChunks;
         [ReadOnly] public float inMotionHalfSpeed;
@@ -80,9 +66,9 @@ namespace SolidSpace.Entities.Physics.Rigidbody
 
                             var otherCenter = FloatMath.GetBoundsCenter(otherBounds);
                             var otherShape = inColliders.shapes[otherIndex];
-                            var shapeA = new ColliderShape(thisCenter, thisShape.rotation, thisShape.size);
-                            var shapeB = new ColliderShape(otherCenter, otherShape.rotation, otherShape.size);
-                            if (!ShapesIntersect(shapeA, shapeB))
+                            var shapeA = new CenterRotationSize(thisCenter, thisShape.rotation, thisShape.size);
+                            var shapeB = new CenterRotationSize(otherCenter, otherShape.rotation, otherShape.size);
+                            if (!CollisionResolver.ShapesIntersect(shapeA, shapeB))
                             {
                                 continue;
                             }
@@ -135,9 +121,9 @@ namespace SolidSpace.Entities.Physics.Rigidbody
 
                                 var otherCenter = FloatMath.GetBoundsCenter(otherBounds);
                                 var otherShape = inColliders.shapes[otherIndex];
-                                var shapeA = new ColliderShape(thisCenter, thisShape.rotation, thisShape.size);
-                                var shapeB = new ColliderShape(otherCenter, otherShape.rotation, otherShape.size);
-                                if (!ShapesIntersect(shapeA, shapeB))
+                                var shapeA = new CenterRotationSize(thisCenter, thisShape.rotation, thisShape.size);
+                                var shapeB = new CenterRotationSize(otherCenter, otherShape.rotation, otherShape.size);
+                                if (!CollisionResolver.ShapesIntersect(shapeA, shapeB))
                                 {
                                     continue;
                                 }
@@ -160,81 +146,6 @@ namespace SolidSpace.Entities.Physics.Rigidbody
 
                 outMotion[thisIndex] = motion;
             }
-        }
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private readonly bool ShapesIntersect(ColliderShape shapeA, ColliderShape shapeB)
-        {
-            var child = WorldToLocalSpace(shapeA, shapeB);
-            if (SeparateAxisExists(shapeA.size, child))
-            {
-                return false;
-            }
-
-            child = WorldToLocalSpace(shapeB, shapeA);
-            if (SeparateAxisExists(shapeB.size, child))
-            {
-                return false;
-            }
-
-            return true;
-        }
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private readonly ColliderShape WorldToLocalSpace(ColliderShape parent, ColliderShape child)
-        {
-            child.rotation -= parent.rotation;
-            child.center -= parent.center;
-            FloatMath.SinCos(-parent.rotation, out var sin, out var cos);
-            child.center = FloatMath.Rotate(child.center, sin, cos);
-            
-            return child;
-        }
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private readonly bool SeparateAxisExists(float2 bounds, ColliderShape shape)
-        {
-            FloatMath.SinCos(shape.rotation, out var sin, out var cos);
-            var halfSize = shape.size * 0.5f;
-            var p0 = shape.center + FloatMath.Rotate(-halfSize.x, -halfSize.y, sin, cos);
-            var p1 = shape.center + FloatMath.Rotate(-halfSize.x, +halfSize.y, sin, cos);
-            var p2 = shape.center + FloatMath.Rotate(+halfSize.x, +halfSize.y, sin, cos);
-            var p3 = shape.center + FloatMath.Rotate(+halfSize.x, -halfSize.y, sin, cos);
-            var halfBounds = bounds * 0.5f;
-            
-            if (AllGreaterOrEqual(+halfBounds.x, shape.center.x, p0.x, p1.x, p2.x, p3.x))
-            {
-                return true;
-            }
-
-            if (AllLessOrEqual(-halfBounds.x, shape.center.x, p0.x, p1.x, p2.x, p3.x))
-            {
-                return true;
-            }
-
-            if (AllGreaterOrEqual(+halfBounds.y, shape.center.y, p0.y, p1.y, p2.y, p3.y))
-            {
-                return true;
-            }
-
-            if (AllLessOrEqual(-halfBounds.y, shape.center.y, p0.y, p1.y, p2.y, p3.y))
-            {
-                return true;
-            }
-
-            return false;
-        }
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private readonly bool AllGreaterOrEqual(float t, float v0, float v1, float v2, float v3, float v4)
-        {
-            return (v0 >= t) && (v1 >= t) && (v2 >= t) && (v3 >= t) && (v4 >= t);
-        }
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private readonly bool AllLessOrEqual(float t, float v0, float v1, float v2, float v3, float v4)
-        {
-            return (v0 <= t) && (v1 <= t) && (v2 <= t) && (v3 <= t) && (v4 <= t);
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
